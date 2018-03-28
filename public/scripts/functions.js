546f9c4e820a24f7d312174a351342c5e3d082fa
John Wu <john.wu@mail.mcgill.ca>
2018-03-28 03:00:40 -0400

    Added azure student account url

--- public/scripts/functions.js
+++ public/scripts/functions.js
@@ -1,4 +1,5 @@
-// Retrieve AP Key - https://azure.microsoft.com/en-ca/try/cognitive-services/
+// Create Azure Account - https://azure.microsoft.com/en-us/free/free-account-students-faq/
+// Retrieve API Key - https://azure.microsoft.com/en-ca/try/cognitive-services/
 // API Documentation - https://westus.dev.cognitive.microsoft.com/docs/services/TextAnalytics.V2.0/operations/56f30ceeeda5650db055a3c9
 function submitComment(commentControl) {
     // TODO - Call API
